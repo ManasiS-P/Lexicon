957cca632e9efdc5f4709c8957513d6ff2ac6131
Manasi Patil <manasipatil231195@gmail.com>
2026-01-29 16:33:25 +0100

    Neural Network Model

---
+++ Machine_Learning/PART_4/nn.py
@@ -0,0 +1,29 @@
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+# Random numbers
+
+# generator
+# Discriminator  <- This
+
+
+def build_discriminator(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']):
+    model = Sequential()
+
+    # Input layer
+    model.add(Dense(128, activation = 'relu', input_shape=(1,)))
+
+    # Hidden layers
+    model.add(Dense(64, activation = 'relu'))
+    model.add(Dense(32, activation = 'relu'))
+
+
+
+    # Output layer
+    model.add(Dense(1, activation = 'sigmoid'))
+
+    model.compile(optimizer=optimizer, loss=loss, metrics = metrics)
+
+    return model
